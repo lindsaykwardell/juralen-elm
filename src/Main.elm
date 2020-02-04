f9af6704eb4ed4ee0cd146ce32db8598a6405244
Lindsay Wardell <three060@gmail.com>
2020-02-04 15:33:37 -0800

    Added login page and temp home page

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,15 @@
-module Main exposing (main)
+port module Main exposing (main)
 
 import Browser exposing (Document)
 import Browser.Navigation as Nav
 import Game
 import Game.Core as Core
-import Html exposing (div, text)
-import Html.Attributes exposing (class)
+import Html exposing (Attribute, button, div, form, h2, input, text)
+import Html.Attributes exposing (class, placeholder, type_, value)
+import Html.Events exposing (onClick, onInput, onSubmit, preventDefaultOn)
 import Http
-import Json.Decode exposing (Decoder, field, string)
+import Json.Decode as Decode exposing (Decoder, field, string)
+import Json.Encode as Encode
 import Juralen.Player exposing (NewPlayer)
 import Lobby
 import Process
@@ -19,28 +21,57 @@ import Url exposing (Url)
 ---- MODEL ----
 
 
+type alias Init =
+    { isProd : Bool
+    , token : String
+    }
+
+
 type Page
     = Splash
-    | Home
     | Login
+    | Home
     | Lobby Lobby.Model
     | Game Core.Model
 
 
 type alias Model =
     { page : Page
+    , username : String
+    , password : String
+    , token : String
     , newPlayers : List NewPlayer
     }
 
 
-init : Bool -> ( Model, Cmd Msg )
-init isProd =
-    ( { page = Splash, newPlayers = [] }, delay 2000.0 (ChangePage (Lobby (Tuple.first Lobby.init))) )
+type alias LoginPayload =
+    { token : String
+    , username : String
+    }
+
+
+defaultModel : Model
+defaultModel =
+    { page = Splash
+    , username = ""
+    , password = ""
+    , token = ""
+    , newPlayers = []
+    }
+
+
+init : Init -> ( Model, Cmd Msg )
+init payload =
+    if payload.isProd then
+        ( { defaultModel | token = payload.token }, delay 2000.0 (ChangePage (Lobby (Tuple.first Lobby.init))) )
+
+    else
+        -- ( { defaultModel | page = Login, token = payload.token }, Cmd.none )
+        ( { defaultModel | token = payload.token }, delay 0 (ChangePage Login) )
 
 
 
 ---- FUNCTIONS ----
-
 
 
 delay : Float -> msg -> Cmd msg
@@ -49,12 +80,28 @@ delay time msg =
         |> Task.perform (\_ -> msg)
 
 
+preventDefaultOnSubmit : msg -> Attribute msg
+preventDefaultOnSubmit msg =
+    preventDefaultOn "submit" (Decode.succeed ( msg, True ))
+
+
+decodeLoginPayload : Decoder LoginPayload
+decodeLoginPayload =
+    Decode.map2 LoginPayload
+        (field "token" string)
+        (field "username" string)
+
+
 
 ---- UPDATE ----
 
 
 type Msg
-    = ChangePage Page
+    = EnterUsername String
+    | EnterPassword String
+    | AttemptLogin
+    | LoginResult (Result Http.Error LoginPayload)
+    | ChangePage Page
     | GotLobbyMsg Lobby.Msg
     | GotGameMsg Game.Msg
 
@@ -62,13 +109,71 @@ type Msg
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        EnterUsername username ->
+            ( { model | username = username }, Cmd.none )
+
+        EnterPassword password ->
+            ( { model | password = password }, Cmd.none )
+
+        AttemptLogin ->
+            ( { model | password = "" }
+            , Http.post
+                { url = "/api/login"
+                , body =
+                    Http.jsonBody
+                        (Encode.object
+                            [ ( "username", Encode.string model.username )
+                            , ( "password", Encode.string model.password )
+                            ]
+                        )
+                , expect = Http.expectJson LoginResult decodeLoginPayload
+                }
+            )
+
+        LoginResult result ->
+            case result of
+                Err err ->
+                    let
+                        _ =
+                            Debug.log "Error!" err
+                    in
+                    ( model, Cmd.none )
+
+                Ok loginPayload ->
+                    let
+                        _ =
+                            Debug.log "Success!" loginPayload
+                    in
+                    update (ChangePage Home) { model | username = loginPayload.username, token = loginPayload.token }
+
         ChangePage page ->
             case page of
-                Game game ->
-                    ( { model | page = Game (Tuple.first (Game.init model.newPlayers)) }, Cmd.map GotGameMsg (Tuple.second (Game.init model.newPlayers)) )
+                Login ->
+                    if String.length model.token == 0 then
+                        ( { model | page = Login }, Cmd.none )
+
+                    else
+                        -- Validate token
+                        ( { model | page = Login }
+                        , Http.post
+                            { url = "/api/auth"
+                            , body =
+                                Http.jsonBody
+                                    (Encode.object
+                                        [ ( "token", Encode.string model.token ) ]
+                                    )
+                            , expect = Http.expectJson LoginResult decodeLoginPayload
+                            }
+                        )
+
+                Home ->
+                    ( { model | page = Home }, storeJwt model.token )
 
                 Lobby lobby ->
                     ( { model | page = Lobby lobby }, Cmd.map GotLobbyMsg (Tuple.second Lobby.init) )
+
+                Game _ ->
+                    ( { model | page = Game (Tuple.first (Game.init model.newPlayers)) }, Cmd.map GotGameMsg (Tuple.second (Game.init model.newPlayers)) )
 
                 _ ->
                     ( model, Cmd.none )
@@ -106,6 +211,13 @@ toGame model ( game, cmd ) =
 
 
 
+---- PORTS ----
+
+
+port storeJwt : String -> Cmd msg
+
+
+
 ---- VIEW ----
 
 
@@ -115,16 +227,32 @@ view model =
     , body =
         [ case model.page of
             Splash ->
-                div [ class "splash" ] [ Html.h1 [] [ text "JURALEN" ] ]
+                div [ class "splash" ] [ Html.h1 [ class "text-white" ] [ text "JURALEN" ] ]
+
+            Login ->
+                div [ class "login" ]
+                    [ div [ class "border-2 bg-gray-600 p-3 xl:w-1/5 lg:w-1/3 md:w-1/2 sm:w-2/3" ]
+                        [ h2 [ class "m-3" ] [ text "Log In" ]
+                        , form [ preventDefaultOnSubmit AttemptLogin ]
+                            [ input [ class "rounded my-3 w-full p-2", type_ "text", placeholder "Username", onInput EnterUsername ] []
+                            , input [ class "rounded my-3 w-full p-2", type_ "password", placeholder "Password", onInput EnterPassword ] []
+                            , input [ class "px-3 py-2 bg-blue-300 hover:bg-blue-500 pointer rounded", type_ "submit" ] [ text "Log In" ]
+                            ]
+                        ]
+                    ]
+
+            Home ->
+                div [ class "login" ]
+                    [ div [ class "border-2 bg-gray-600 p-3 xl:w-1/5 lg:w-1/3 md:w-1/2 sm:w-2/3" ]
+                        [ button [ onClick (ChangePage (Lobby (Tuple.first Lobby.init))) ] [ text "Enter Lobby" ]
+                        ]
+                    ]
 
             Lobby lobby ->
                 Lobby.view lobby |> Html.map GotLobbyMsg
 
             Game game ->
                 Game.view game |> Html.map GotGameMsg
-
-            _ ->
-                div [] []
         ]
     }
 
@@ -133,7 +261,7 @@ view model =
 ---- PROGRAM ----
 
 
-main : Program Bool Model Msg
+main : Program Init Model Msg
 main =
     Browser.document
         { init = init
--- src/index.js
+++ src/index.js
@@ -1,43 +1,58 @@
-import "./main.css";
-import { Elm } from "./Main.elm";
-import * as serviceWorker from "./serviceWorker";
+import "./main.css"
+import { Elm } from "./Main.elm"
+import * as serviceWorker from "./serviceWorker"
 
-import { Server, Model, Response } from "miragejs";
+import { Server, Model, Response } from "miragejs"
+import uuid from "uuid/v4"
 
 new Server({
-  models: {
-    user: Model
-  },
-  routes() {
-    this.get("/test", (schema, request) => {
-      return { name: "bob" };
-    });
+    models: {
+        user: Model
+    },
+    routes() {
+        this.get("/test", (schema, request) => {
+            return { name: "bob" }
+        })
 
-    this.post("/api/login", (schema, request) => {
-      debugger
-      let credentials = JSON.parse(request.requestBody)
+        this.post("/api/login", (schema, request) => {
+            let credentials = JSON.parse(request.requestBody)
 
-      const user = schema.users.all().find(user => user.name === credentials.name)
+            const { models: users } = schema.users.all()
 
-      if (!user || user.password !== credentials.password) {
-        return new Response(400, {}, { errors: ["Invalid login"] })
-      }
-      else {
-        return user
-      }
-    });
-  },
-  seeds(server) {
-    server.create("user", { name: "Yagaboosh", password: "password123" });
-  }
-});
+            const user = users.find(user => user.name === credentials.username)
 
-Elm.Main.init({
-  flags: process.env.NODE_ENV === "production",
-  node: document.getElementById("root")
-});
+            if (!user || user.password !== credentials.password) {
+                return new Response(400, {}, { errors: ["Invalid login"] })
+            } else {
+                const token = uuid()
+
+                return { username: user.name, token }
+            }
+        })
+
+        this.post("/api/auth", (schema, request) => {
+            let credentials = JSON.parse(request.requestBody)
+            return { username: "Yagaboosh", token: credentials.token }
+        })
+    },
+    seeds(server) {
+        server.create("user", { name: "Yagaboosh", password: "password123" })
+    }
+})
+
+const app = Elm.Main.init({
+    flags: {
+        isProd: process.env.NODE_ENV === "production",
+        token: localStorage.getItem("token") || ""
+    },
+    node: document.getElementById("root")
+})
+
+app.ports.storeJwt.subscribe(token => {
+    localStorage.setItem(`token`, token)
+})
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
