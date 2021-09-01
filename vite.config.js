5c13842593914e6edce99b682a103450bf77f699
Lindasy Wardell <three060@gmail.com>
2021-09-01 06:28:32 -0700

    Adding PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite"
 import elmPlugin from "vite-plugin-elm"
 import vue from "@vitejs/plugin-vue"
+import { VitePWA } from "vite-plugin-pwa"
 
 export default defineConfig({
-    plugins: [elmPlugin(), vue({ customElement: true })],
+    plugins: [elmPlugin(), vue({ customElement: true }), VitePWA({
+        includeAssets: ['/img/**'],
+        manifest: {
+            name: 'Juralen',
+            short_name: 'Juralen',
+            description: 'Turn-Based Strategy Game',
+            theme_color: '#333'
+          }
+    })],
 })
