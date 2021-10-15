83345cd3e1e56fc291d0cf353910fc2c4d4292e0
Lindasy Wardell <three060@gmail.com>
2021-10-14 19:27:50 -0700

    Add registerSW call

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-import "./style.css";
-import './src/Components'
-import app from './src/app'
+import "./style.css"
+import "./src/Components"
+import app from "./src/app"
+import { registerSW } from "virtual:pwa-register"
+
+const updateSW = registerSW({
+    // onNeedRefresh() {},
+    // onOfflineReady() {},
+})
 
 setTimeout(() => {
-  app.ports.authStatus.send(true);
+    app.ports.authStatus.send(true)
 }, 2000)
